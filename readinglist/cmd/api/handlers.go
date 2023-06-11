7d34ea9d8731cd363e3abdf2055105c33ae2dff0
MTomys <michalltomys@protonmail.ch>
2023-06-11 17:39:21 +0200

    add course progress

--- readinglist/cmd/api/handlers.go
+++ readinglist/cmd/api/handlers.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
-	"readinglist/internal/data"
 	"strconv"
-	"time"
+
+	"readinglist/internal/data"
 )
 
 func (app *application) healthcheck(w http.ResponseWriter, r *http.Request) {
@@ -14,49 +15,36 @@ func (app *application) healthcheck(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
 		return
 	}
+
 	data := map[string]string{
 		"status":      "available",
 		"environment": app.config.env,
 		"version":     version,
 	}
 
-	jsonData, err := json.Marshal(data)
+	js, err := json.Marshal(data)
 	if err != nil {
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
 	}
 
-	jsonData = append(jsonData, '\n')
+	js = append(js, '\n')
+
 	w.Header().Set("Content-Type", "application/json")
 
-	w.Write(jsonData)
+	w.Write(js)
 }
 
 func (app *application) getCreateBooksHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		fmt.Fprintln(w, "Display a list of the books on the reading list")
 
-		books := []data.Book{
-			{
-				ID:        1,
-				CreatedAt: time.Now(),
-				Title:     "Thus spoke zarathustra",
-				Published: 1912,
-				Pages:     300,
-				Genres:    []string{"Fiction", "Thriller"},
-				Version:   1,
-			},
-			{
-				ID:        2,
-				CreatedAt: time.Now(),
-				Title:     "XD",
-				Published: 2019,
-				Pages:     300,
-				Genres:    []string{"Fiction", "Thriller"},
-				Version:   1,
-			},
+	if r.Method == http.MethodGet {
+		books, err := app.models.Books.GetAll()
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
 		}
 
-		if err := app.writeJSON(w, http.StatusOK, envelope{"books": books}); err != nil {
+		if err := app.writeJSON(w, http.StatusOK, envelope{"books": books}, nil); err != nil {
 			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 			return
 		}
@@ -68,56 +56,98 @@ func (app *application) getCreateBooksHandler(w http.ResponseWriter, r *http.Req
 			Published int      `json:"published"`
 			Pages     int      `json:"pages"`
 			Genres    []string `json:"genres"`
-			Rating    float64  `json:"rating"`
+			Rating    float32  `json:"rating"`
 		}
 
-		app.readJSON(w, r, &input)
+		err := app.readJSON(w, r, &input)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
 
-		fmt.Fprintf(w, "%v\n", input)
+		book := &data.Book{
+			Title:     input.Title,
+			Published: input.Published,
+			Pages:     input.Pages,
+			Genres:    input.Genres,
+			Rating:    input.Rating,
+		}
+
+		err = app.models.Books.Insert(book)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+
+		headers := make(http.Header)
+		headers.Set("Location", fmt.Sprintf("v1/books/%d", book.ID))
+
+		// Write the JSON response with a 201 Created status code and the Location header set.
+		err = app.writeJSON(w, http.StatusCreated, envelope{"book": book}, headers)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
 	}
 }
 
 func (app *application) getUpdateDeleteBooksHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
-		{
-			app.getBook(w, r)
-		}
+		app.getBook(w, r)
 	case http.MethodPut:
-		{
-			app.updateBook(w, r)
-		}
+		app.updateBook(w, r)
 	case http.MethodDelete:
-		{
-			app.deleteBook(w, r)
-		}
+		app.deleteBook(w, r)
 	default:
-		{
-			http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
-		}
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
 	}
 }
 
 func (app *application) getBook(w http.ResponseWriter, r *http.Request) {
-	id := getIdFromRequest(w, r)
-	book := data.Book{
-		ID:        id,
-		CreatedAt: time.Now(),
-		Title:     "Echoes in the darkness",
-		Published: 2019,
-		Pages:     300,
-		Genres:    []string{"Fiction", "Thriller"},
-		Version:   1,
+	id := r.URL.Path[len("/v1/books/"):]
+	idInt, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
 	}
 
-	if err := app.writeJSON(w, http.StatusOK, envelope{"book": book}); err != nil {
+	book, err := app.models.Books.Get(idInt)
+	if err != nil {
+		switch {
+		case errors.Is(err, errors.New("record not found")):
+			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		default:
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		}
+		return
+	}
+
+	if err := app.writeJSON(w, http.StatusOK, envelope{"book": book}, nil); err != nil {
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 }
 
 func (app *application) updateBook(w http.ResponseWriter, r *http.Request) {
-	id := getIdFromRequest(w, r)
+	id := r.URL.Path[len("/v1/books/"):]
+	idInt, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	book, err := app.models.Books.Get(idInt)
+	if err != nil {
+		switch {
+		case errors.Is(err, errors.New("record not found")):
+			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		default:
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		}
+		return
+	}
+
 	var input struct {
 		Title     *string  `json:"title"`
 		Published *int     `json:"published"`
@@ -126,47 +156,66 @@ func (app *application) updateBook(w http.ResponseWriter, r *http.Request) {
 		Rating    *float32 `json:"rating"`
 	}
 
-	book := data.Book{
-		ID:        id,
-		CreatedAt: time.Now(),
-		Title:     "Echoes in the darkness",
-		Published: 2019,
-		Pages:     300,
-		Genres:    []string{"Fiction", "Thriller"},
-		Rating:    4.5,
-		Version:   1,
+	err = app.readJSON(w, r, &input)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
 	}
-
-	app.readJSON(w, r, &input)
 
 	if input.Title != nil {
 		book.Title = *input.Title
 	}
+
 	if input.Published != nil {
 		book.Published = *input.Published
 	}
+
 	if input.Pages != nil {
 		book.Pages = *input.Pages
 	}
+
 	if len(input.Genres) > 0 {
 		book.Genres = input.Genres
 	}
+
 	if input.Rating != nil {
 		book.Rating = *input.Rating
 	}
-	fmt.Fprintf(w, "%v\n", book)
+
+	err = app.models.Books.Update(book)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	if err := app.writeJSON(w, http.StatusOK, envelope{"book": book}, nil); err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
 }
 
 func (app *application) deleteBook(w http.ResponseWriter, r *http.Request) {
-	id := getIdFromRequest(w, r)
-	fmt.Fprintf(w, "Delete the book with id: %d", id)
-}
-
-func getIdFromRequest(w http.ResponseWriter, r *http.Request) int64 {
 	id := r.URL.Path[len("/v1/books/"):]
-	idInt, err := strconv.ParseInt(id, 0, 64)
+	idInt, err := strconv.ParseInt(id, 10, 64)
 	if err != nil {
 		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
 	}
-	return idInt
+
+	err = app.models.Books.Delete(idInt)
+	if err != nil {
+		switch {
+		case errors.Is(err, errors.New("record not found")):
+			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		default:
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		}
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"message": "book successfully deleted"}, nil)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
 }
